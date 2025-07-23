675439d7c6a45ea345d7add00fb968fe047089a4
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-07-23 02:37:54 +0000

    mapos

--- qr.rb
+++ qr.rb
@@ -1,6 +1,8 @@
 require "rqrcode"
 require "active_support/all"
 
+
+
 pp "Please enter 1, 2, 3, or 4."
 
 command = gets.chomp
@@ -17,10 +19,10 @@ qrcode = RQRCode::QRCode.new(url)
 # code for joining a wifi network
 #qrcode = RQRCode::QRCode.new("WIFI:T:WPA;S:My wifi network;P:supersecretpassword;;")
 
-
-
 # Use the .as_png method to create a 500 pixels by 500 pixels image
 png = qrcode.as_png({ :size => 500 })
 
 # Write the image data to a file
 IO.binwrite("sometext.png", png.to_s)
+
+
