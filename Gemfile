4b3524a10a2f7f3c1e625ae042f930119ce41a22
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-07-23 01:35:47 +0000

    got the linkedin link to save `

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
 
 gem "rqrcode"
+gem "activesupport"
+gem "awesome_print"
+gem "pry-byebug"
--- qr.rb
+++ qr.rb
@@ -1,1 +1,26 @@
-pp "hello, world!"
+require "rqrcode"
+require "active_support/all"
+
+pp "Please enter 1, 2, 3, or 4."
+
+command = gets.chomp
+
+if command == "1" 
+pp "Provide the URL that you would like to transform into a QR code."
+  url = gets.chomp
+end
+
+
+# Use the RQRCode::QRCode class to encode some text
+qrcode = RQRCode::QRCode.new(url)
+
+# code for joining a wifi network
+#qrcode = RQRCode::QRCode.new("WIFI:T:WPA;S:My wifi network;P:supersecretpassword;;")
+
+
+
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = qrcode.as_png({ :size => 500 })
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
